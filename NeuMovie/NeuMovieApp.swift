4aac6b09bd0500303251af27adee0c508401f64d
AbdoFoda <abdofoda@stud.fci-cu.edu.eg>
2025-01-29 21:22:38 +0100

    Initial Commit

---
+++ NeuMovie/NeuMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuMovieApp.swift
+//  NeuMovie
+//
+//  Created by Abdulrahman Foda on 29.01.25.
+//
+
+import SwiftUI
+
+@main
+struct NeuMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeuMovieTests/NeuMovieTests.swift
@@ -0,0 +1,17 @@
+//
+//  NeuMovieTests.swift
+//  NeuMovieTests
+//
+//  Created by Abdulrahman Foda on 29.01.25.
+//
+
+import Testing
+@testable import NeuMovie
+
+struct NeuMovieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
